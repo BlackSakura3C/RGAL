69fd8ac5413d6474a3a12e588afc40ea1ce49ae0
BlackSakura3C <250820552@qq.com>
2022-10-03 23:03:55 +0800

    仓库中删除不需要build目录

--- build/TestConfig.h
+++
@@ -1,6 +0,0 @@
-// the configured options and settings for Riot Game Application For League of legends(RGAL)
-#define RGAL_VERSION_MAJOR 1
-#define RGAL_VERSION_MINOR 0
-
-#define USE_GLOG
-#define USE_IMGUI
